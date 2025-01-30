fb0ab9d96217c290ee685455a98800d3b5e80f22
Watanyu onnum <ipae.wo@gmail.com>
2025-01-31 04:19:17 +0700

    firstCommit

---
+++ chat_javaProj/src/chat_javaproj/Chat_javaProj.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package chat_javaproj;
+
+/**
+ *
+ * @author ipae
+ */
+public class Chat_javaProj {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       logIn login = new logIn();
+       
+       login.setVisible(true);
+    }
+    
+}
---
+++ chat_javaProj/src/chat_javaproj/logIn.java
@@ -0,0 +1,312 @@
+package chat_javaproj;
+
+import java.awt.Color;
+import java.awt.Desktop;
+import java.net.URI;
+import javax.swing.BorderFactory;
+import javax.swing.border.Border;
+import org.w3c.dom.css.RGBColor;
+
+
+public class logIn extends javax.swing.JFrame {
+    
+    rsuApp app = new rsuApp();
+    
+    public logIn() {
+        initComponents();
+        
+    }
+
+
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel2 = new javax.swing.JLabel();
+        txtUsername = new javax.swing.JTextField();
+        btnLogin = new javax.swing.JButton();
+        btnExit = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        txtPassword = new javax.swing.JPasswordField();
+        lbHelp = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/chat_javaproj/rsuIcon.png"))); // NOI18N
+        jLabel2.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+
+        txtUsername.setColumns(20);
+        txtUsername.setFont(new java.awt.Font("Helvetica Neue", 0, 14)); // NOI18N
+        txtUsername.setBorder(javax.swing.BorderFactory.createEtchedBorder(javax.swing.border.EtchedBorder.RAISED, java.awt.Color.darkGray, java.awt.Color.white));
+        txtUsername.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                txtUsernameFocusGained(evt);
+            }
+        });
+        txtUsername.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtUsernameActionPerformed(evt);
+            }
+        });
+        txtUsername.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                txtUsernameKeyReleased(evt);
+            }
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                txtUsernameKeyTyped(evt);
+            }
+        });
+
+        btnLogin.setFont(new java.awt.Font("Helvetica Neue", 0, 14)); // NOI18N
+        btnLogin.setText("Login");
+        btnLogin.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnLoginActionPerformed(evt);
+            }
+        });
+
+        btnExit.setFont(new java.awt.Font("Helvetica Neue", 0, 14)); // NOI18N
+        btnExit.setText("Exit");
+        btnExit.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnExitActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setFont(new java.awt.Font("Silom", 0, 24)); // NOI18N
+        jLabel1.setText("RSU Login");
+
+        jLabel3.setText("Username");
+
+        jLabel4.setText("Password");
+
+        txtPassword.setBorder(javax.swing.BorderFactory.createEtchedBorder(java.awt.Color.black, new java.awt.Color(255, 255, 255)));
+        txtPassword.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                txtPasswordFocusGained(evt);
+            }
+        });
+        txtPassword.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                txtPasswordKeyReleased(evt);
+            }
+        });
+
+        lbHelp.setFont(new java.awt.Font("Helvetica Neue", 2, 13)); // NOI18N
+        lbHelp.setForeground(new java.awt.Color(51, 51, 255));
+        lbHelp.setText("need help?");
+        lbHelp.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        lbHelp.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                lbHelpMouseClicked(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                lbHelpMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                lbHelpMouseExited(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jLabel2)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(86, 86, 86)
+                .addComponent(jLabel1))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                    .addComponent(jLabel4, javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel3, javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(txtUsername)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(btnLogin)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(btnExit))
+                    .addComponent(txtPassword)
+                    .addComponent(lbHelp, javax.swing.GroupLayout.Alignment.LEADING))
+                .addGap(32, 32, 32))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 323, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, 0)
+                .addComponent(jLabel1)
+                .addGap(27, 27, 27)
+                .addComponent(jLabel3)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(txtUsername, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jLabel4)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(txtPassword, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(lbHelp)
+                .addGap(35, 35, 35)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnLogin)
+                    .addComponent(btnExit))
+                .addGap(75, 75, 75))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+    // ***--------Bordor Color---------****
+    Border borderW= BorderFactory.createLineBorder(Color.WHITE, 5);
+    Border borderR= BorderFactory.createLineBorder(Color.RED, 2);
+    // ***--------Variable-------------***
+    String Input = "",InputPW = "";
+    int i = 0;
+    
+    private void btnLoginActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLoginActionPerformed
+        Input = txtUsername.getText();
+        InputPW = txtPassword.getText();
+        if ((Input.length() + InputPW.length()) == 20) {
+            this.setVisible(false);
+            app.setVisible(true);
+            app.setLocationRelativeTo(this);
+            
+        }
+    }//GEN-LAST:event_btnLoginActionPerformed
+
+    private void txtUsernameActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtUsernameActionPerformed
+
+    }//GEN-LAST:event_txtUsernameActionPerformed
+
+    private void btnExitActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnExitActionPerformed
+        System.exit(0);
+    }//GEN-LAST:event_btnExitActionPerformed
+    
+    private void txtUsernameKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtUsernameKeyTyped
+
+    }//GEN-LAST:event_txtUsernameKeyTyped
+
+    private void txtUsernameKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtUsernameKeyReleased
+        Input = txtUsername.getText();
+        
+        validationInput(Input);
+        System.out.println(Input.length());
+    }//GEN-LAST:event_txtUsernameKeyReleased
+
+    private void txtPasswordKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtPasswordKeyReleased
+        InputPW = txtPassword.getText();
+        
+        validationInputPW(InputPW);
+    }//GEN-LAST:event_txtPasswordKeyReleased
+
+    private void txtUsernameFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_txtUsernameFocusGained
+        Input = txtUsername.getText();
+        Border borderTouch= BorderFactory.createLineBorder(Color.BLACK, 2);
+        boolean on_off = Input.isEmpty();
+        
+        if (on_off) {
+            txtUsername.setBorder(borderTouch);
+        }
+    }//GEN-LAST:event_txtUsernameFocusGained
+
+    private void txtPasswordFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_txtPasswordFocusGained
+        Input = txtPassword.getText();
+        Border borderTouch= BorderFactory.createLineBorder(Color.BLACK, 2);
+        boolean on_off = Input.isEmpty();
+        
+        if (on_off) {
+            txtPassword.setBorder(borderTouch);
+        }
+    }//GEN-LAST:event_txtPasswordFocusGained
+
+    private void lbHelpMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lbHelpMouseEntered
+        lbHelp.setText("<html><u>need help?</u></html>");
+    }//GEN-LAST:event_lbHelpMouseEntered
+
+    private void lbHelpMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lbHelpMouseExited
+        lbHelp.setText("need help?");
+    }//GEN-LAST:event_lbHelpMouseExited
+
+    private void lbHelpMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lbHelpMouseClicked
+        try {
+                    Desktop.getDesktop().browse(new URI("https://www.rsu.ac.th/")); // เปลี่ยน URL ได้
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+    }//GEN-LAST:event_lbHelpMouseClicked
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(logIn.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(logIn.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(logIn.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(logIn.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new logIn().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnExit;
+    private javax.swing.JButton btnLogin;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel lbHelp;
+    private javax.swing.JPasswordField txtPassword;
+    private javax.swing.JTextField txtUsername;
+    // End of variables declaration//GEN-END:variables
+
+        
+    private void validationInput(String Input) {
+        String input = Input;
+        
+        int lengthInput = input.length();
+        
+        if (lengthInput == 7) {         //6708767
+            txtUsername.setBorder(borderW);
+        } 
+        else
+        txtUsername.setBorder(borderR);
+    }
+
+    private void validationInputPW(String InputPW) {
+        String input = InputPW;
+        
+        int lengthInput = input.length();
+        
+        if (lengthInput == 13) {         // 1200901241039
+            txtPassword.setBorder(borderW);
+        } 
+        else
+        txtPassword.setBorder(borderR);
+    }
+}
